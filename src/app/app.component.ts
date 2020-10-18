5d15a43e50d94efbb53797205a0a05aee21386bb
Joe Cui <joe.cui@bpanz.com>
2020-10-18 16:44:14 +1000

    Control signedin state using RxJs BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'emailclient';
+  signedin = false;
+  constructor(private authService: AuthService) {}
+
+  // listen to observable on start
+  ngOnInit() {
+    this.authService.signedin$.subscribe((signedin) => {
+      this.signedin = signedin;
+    });
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -23,6 +25,10 @@ export class AuthService {
 
   rootUrl = 'https://api.angular-email.com';
 
+  // BehaviorSubject is both observable and observer, it can take in a default value (false) and emit new value immediately after new subscriber subscribes
+  // adding an optional "$" sign to label it as an "observable"
+  signedin$ = new BehaviorSubject(false); // not signed in by default (false)
+
   // this function will return an observable
   usernameAvailable(username: string) {
     // generic annotation <any>: getting any type of response from request
@@ -37,9 +43,14 @@ export class AuthService {
 
   // receive values from signup form
   signup(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(
-      `${this.rootUrl}/auth/signup`,
-      credentials
-    );
+    return this.http
+      .post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
+      .pipe(
+        // if an error coming out of the observable, it will skip the tap() operator
+        tap(() => {
+          // make the signedin$ BehaviorSubject emit a new value "true"
+          this.signedin$.next(true); // emit "true" to the subscriber (app.component) upon successful signup
+        })
+      );
   }
 }
