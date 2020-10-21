7d41333d9f3920674c4105b5f6b37e531d42b2e5
Joe Cui <joe.cui@bpanz.com>
2020-10-21 11:09:20 +1000

    Wiring up the modal component

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  @Output() dismiss = new EventEmitter(); // emit a dismiss event to close modal
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    // append modal directly under body element
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onDismissClick() {
+    this.dismiss.emit(); // emit "close modal" event
+  }
+}
