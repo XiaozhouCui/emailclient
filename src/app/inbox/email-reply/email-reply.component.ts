2febd83298fe3d0d2688435ee83611a1477c6b05
Joe Cui <joe.cui@bpanz.com>
2020-10-22 20:12:08 +1000

    Building email-reply template and class

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent implements OnInit {
+  showModal = false;
+  email: Email;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit(email: Email) {}
 }
